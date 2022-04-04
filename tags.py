d3ad008d7e341194c26aa75e2dff24ba92bd4432
Nicolò Bartolini <nicolobartolinii@gmail.com>
2022-04-04 17:42:33 +0200

    tag command

--- tags.py
+++ tags.py
@@ -25,6 +25,40 @@ class Tags(commands.Cog):
     async def tags(self, ctx: commands):
         pass
 
+    @commands.command(name='tag', aliases=['t'])
+    async def tag(self, ctx: commands.Context, tag_name: str, card_code: str = None):
+        if not is_user_registered(ctx.author):
+            await ctx.send('You should first register an account using the `start` command.')
+            return
+        user = users.find_one({'_id': str(ctx.author.id)})
+        user_tags = user['tags']
+        user_inventory = user['inventory']
+        if card_code is None:
+            card_code = user_inventory[-1]
+        card_code = card_code.upper()
+        if card_code not in user_inventory:
+            await ctx.send(f'{ctx.author.mention}, you are not the owner of that card.')
+            return
+        try:
+            user_tags[tag_name]
+        except KeyError:
+            await ctx.send(f'Sorry {ctx.author.mention}, that tag does not exist. You can create a new tag using the `createtag` command.')
+            return
+        for tag in user_tags.keys():
+            tagged_cards = user_tags[tag]
+            if card_code in tagged_cards:
+                users.update_one(
+                    {'_id': str(ctx.author.id)},
+                    {'$pull': {f'tags.{tag}': str(card_code)}}
+                )
+        users.update_one(
+            {'_id': str(ctx.author.id)},
+            {'$push': {f'tags.{tag_name}': str(card_code)}}
+        )
+        card_id = grabbed_cards.find_one({'_id': str(card_code)})['cardId']
+        card_name = cards.find_one({'_id': str(card_id)})['name']
+        await ctx.send(f'{ctx.author.mention}, the **{card_name}** card has been tagged successfully with the `{tag_name}` tag.')
+
     @createtag.error
     async def createtag_error(self, ctx: commands.Context, error):
         await ctx.send('Something went wrong. Please use the `help` command to check the usage of commands.')
